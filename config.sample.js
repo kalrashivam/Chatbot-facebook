518a81d9ba35a519f05f0b82e065c9f1ec9c2562
shivamengineersworkbook <shivamkalra2017@gmail.com>
2018-09-29 19:33:24 +0530

    initial conf

---
+++ config.sample.js
@@ -0,0 +1,12 @@
+module.exports = {
+  FB_PAGE_TOKEN:
+    "EAAEvt7LsAeoBAMnZBnDBzRxUQCJa2oke8YnBlzXvaeDRnHD4G3Jq4CyYMMeaxctojCVK5ZCPJ8FjoxaIbH6q2vcPbPjXwyKcO1d59hynz1v4ArIjdZCkwABOXSPDTAKGFNmuK9qn8ePzspZC4bqAeZB2ZC0bGzwXdjgfsY1L3dBQZDZD",
+    FB_VERIFY_TOKEN: "whatwouldharveydo",
+  FB_APP_SECRET: "",
+    SERVER_URL: "https://chatbot-shivam-fb.herokuapp.com/",
+  GOOGLE_PROJECT_ID: "",
+  DF_LANGUAGE_CODE: "",
+  GOOGLE_CLIENT_EMAIL: "",
+  GOOGLE_PRIVATE_KEY: "",
+    API_AI_CLIENT_ACCESS_TOKEN: "3279477ba6da4ad59bb5b28b3a9115ff"
+};
